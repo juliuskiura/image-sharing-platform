9a7c422c65ea83df1f5a8e78fa068e3ff5557f12
Julius Kiura <juliuswaweru@gmail.com>
2021-10-02 22:31:25 +0300

    new file:   imageShare/account/forms.py 	new file:   imageShare/account/static/css/base.css 	new file:   imageShare/account/templates/account/login.html 	new file:   imageShare/account/templates/base.html 	new file:   imageShare/account/urls.py 	modified:   imageShare/account/views.py 	modified:   imageShare/db.sqlite3 	modified:   imageShare/imageShare/urls.py

--- imageShare/account/views.py
+++ imageShare/account/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
 # Create your views here.
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            contact_data = form.cleaned_data
+            user = authenticate(
+                request, username=contact_data['username'], password=contact_data['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Authenticated successfully')
+                else:
+                    return HttpResponse('Disabled Account')
+            else:
+                return HttpResponse('Invalid login')
+    else:
+        form = LoginForm()
+        context = {
+            'form': form
+        }
+        return render(request, 'account/login.html', context)
